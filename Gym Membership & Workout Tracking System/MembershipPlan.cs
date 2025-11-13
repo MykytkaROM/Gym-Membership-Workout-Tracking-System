2acf15d634e957b54221e1ad4889f733bcab0769
MykytkaROM <garyplayer600@gmail.com>
2025-11-13 18:59:29 +0100

    Added validation for discountRate.

--- Gym Membership & Workout Tracking System/MembershipPlan.cs
+++ Gym Membership & Workout Tracking System/MembershipPlan.cs
@@ -32,9 +32,21 @@ namespace Gym_Membership___Workout_Tracking_System
 
         public decimal Price { get; set; }
 
-        private int _discountRate;//discountRate[0..1] : int
+        private int? _discountRate;//discountRate[0..1] : int
 
-        public int DiscountRate { get; set; }
+        public int? DiscountRate { get => _discountRate; set
+            {
+                if (value == null)
+                {
+                    _discountRate = 0;
+                }
+                if (value < 0 || value > 1) {
+                    throw new ArgumentException("Discount should be a value between 0 and 1");
+                }
+                  _discountRate = value;
+                
+
+            } }
 
         private string _benefits;//benefits : string
 
@@ -46,7 +58,7 @@ namespace Gym_Membership___Workout_Tracking_System
 
                 if (String.IsNullOrEmpty(value))
                 {
-                    throw new ArgumentException("Name of membership can't be empty");
+                    throw new ArgumentException("Benefits can't be empty");
                 }
                 _benefits = value;
 
