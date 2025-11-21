7ae2fe613cfd7278f3a348bf2970140dc0398d09
Robo799 <s30936@pjwstk.edu.pl>
2025-11-21 11:54:58 +0100

    example objects

--- Gym Membership & Workout Tracking System/Program.cs
+++ Gym Membership & Workout Tracking System/Program.cs
@@ -17,6 +17,37 @@
                 new User("Michael","michael@gmail.com","12345678",
                 new Address("Warsaw","Koszykowa",10));
             User.save();*/
+            
+            //ADMIN
+            /*var admin = new Admin(
+                adminLevel: 5,
+                permissions: new List<string> { "Read", "Write" }
+            );
+            
+            foreach (var p in admin.Permissions)
+                Console.WriteLine("- " + p);*/
+            
+            /*admin.ManagePermissions("Delete", add: true);
+            
+            Console.WriteLine("\n...");
+            foreach (var p in admin.Permissions)
+                Console.WriteLine("- " + p);*/
+            
+            /*admin.ManagePermissions("Write", add: false);
+            
+            Console.WriteLine("\n...");
+            foreach (var p in admin.Permissions)
+                Console.WriteLine("- " + p);*/
+            
+            //EntryRecord
+            /*var start = new DateTime(2025, 11, 25, 10, 15, 0);
+            var end   = new DateTime(2025, 11, 25, 11, 45, 0);
+            
+            var entry = new EntryRecord(start, end);
+
+            Console.WriteLine($"Start time: {entry.StartTime}");
+            Console.WriteLine($"End time:   {entry.EndTime}");
+            Console.WriteLine($"Duration:   {entry.Duration}");*/
         }
     }
 }
