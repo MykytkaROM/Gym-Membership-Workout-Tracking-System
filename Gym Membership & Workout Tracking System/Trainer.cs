0a9375607b04c8a56b194d8888d05510699ebb39
Robo799 <s30936@pjwstk.edu.pl>
2025-12-16 17:47:02 +0100

    basic ass

--- Gym Membership & Workout Tracking System/Trainer.cs
+++ Gym Membership & Workout Tracking System/Trainer.cs
@@ -294,5 +294,59 @@ namespace Gym_Membership___Workout_Tracking_System
             trainees.ForEach(trainee => { AddTrainee(trainee); });
             AddTrainerEXT(this);
         }
+        
+        // private readonly List<WorkoutProgram> _workoutPrograms = new List<WorkoutProgram>();
+        //
+        // public List<WorkoutProgram> WorkoutPrograms => _workoutPrograms;
+        
+        private readonly List<WorkoutProgram> _workoutPrograms = new List<WorkoutProgram>();
+
+        public List<WorkoutProgram> WorkoutPrograms
+        {
+            get
+            {
+                return new List<WorkoutProgram>(_workoutPrograms);
+            }
+        }
+        
+        //toDO prodnik help me
+        public bool ContainsWorkoutProgram(WorkoutProgram program)
+        {
+            if (program == null) throw new ArgumentNullException("WorkoutProgram cannot be null");
+            return _workoutPrograms.Contains(program);
+        }
+        
+        public void AddWorkoutProgram(WorkoutProgram program)
+        {
+            if (program == null) throw new ArgumentNullException("WorkoutProgram cannot be null");
+
+            if (_workoutPrograms.Contains(program))
+                throw new ArgumentException("This WorkoutProgram is already in the list");
+
+            if (program.Creator != null && !program.Creator.Equals(this))
+                throw new ArgumentException("WorkoutProgram already has a different creator");
+
+            _workoutPrograms.Add(program);
+            
+            if (program.Creator == null)
+            {
+                program.AddCreator(this);
+            }
+        }
+        
+        public void DeleteWorkoutProgram(WorkoutProgram program)
+        {
+            if (program == null) throw new ArgumentNullException("WorkoutProgram cannot be null");
+
+            if (!_workoutPrograms.Contains(program))
+                throw new ArgumentException("WorkoutProgram is not in the list");
+
+            _workoutPrograms.Remove(program);
+
+            if (program.Creator != null && program.Creator.Equals(this))
+            {
+                program.DeleteCreator(this);
+            }
+        }
     }
 }
---
+++ Gym Membership & Workout Tracking System/WorkoutProgram.cs
@@ -0,0 +1,65 @@
+﻿namespace Gym_Membership___Workout_Tracking_System;
+
+public class WorkoutProgram
+{
+    private string _name;
+    public string Name { get => _name; set {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException("Name cannot be empty");
+            _name = value;
+        }
+    }
+    public string Goal { get; set; }
+    public string Difficulty { get; set; }
+    public int DurationWeeks { get; set; }
+    private Trainer _creator;
+    public Trainer Creator => _creator;
+    
+    public WorkoutProgram(string name, string goal, string difficulty, int durationWeeks, Trainer creator)
+    {
+        Name = name;
+        Goal = goal;
+        Difficulty = difficulty;
+        DurationWeeks = durationWeeks;
+
+        AddCreator(creator);
+    }
+    
+    public void AddCreator(Trainer creator)
+    {
+        if (_creator != null)
+            throw new InvalidOperationException("Creator is already added. If you meant to modify it use EditCreator() instead");
+        if (creator == null)
+            throw new ArgumentNullException("Creator should be not null");
+
+        _creator = creator;
+        
+        if (!creator.ContainsWorkoutProgram(this))
+        {
+            creator.AddWorkoutProgram(this);
+        }
+    }
+    
+    public void EditCreator(Trainer newCreator)
+    {
+        if (newCreator == null) throw new ArgumentNullException("Creator should be not null");
+        if (_creator == null) throw new InvalidOperationException("No creator set. Use AddCreator() first");
+        if (newCreator.Equals(_creator)) return;
+
+        DeleteCreator(_creator);
+        AddCreator(newCreator);
+    }
+
+    public void DeleteCreator(Trainer creator)
+    {
+        if (creator == null) throw new ArgumentNullException("Creator should be not null");
+        if (_creator == null) throw new InvalidOperationException("No creator set. Use AddCreator() first");
+        if (!_creator.Equals(creator)) throw new ArgumentException("Creator specified is different from creator in this program");
+        
+        _creator = null;
+        if (creator.ContainsWorkoutProgram(this))
+        {
+            creator.DeleteWorkoutProgram(this);
+        }
+    }
+}
